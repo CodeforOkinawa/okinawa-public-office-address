6136bc27ddc89b09e59aba7948e8b170e0b361ca
Seiei Higa <hanachin@gmail.com>
2014-05-02 02:13:24 +0900

    ライセンスに問題ありそうなものを全部削除

--- convert-to-tsv.rb
+++
@@ -1,7 +0,0 @@
-require 'csv'
-
-File.open('addresses-utf8.tsv', 'w') {|f|
-  CSV.open("./addresses.csv", "r:CP932:UTF-8", row_sep: "\r\n").each {|r|
-    f.puts r.join("\t")
-  }
-}
--- convert.rb
+++
@@ -1,5 +0,0 @@
-require 'csv'
-
-CSV.open("./addresses-utf8.csv", "w", row_sep:"\r\n", force_quotes: true) {|csv|
-  CSV.open("./addresses.csv", "r:CP932:UTF-8", row_sep: "\r\n").each {|r| csv << r.to_a }
-}
