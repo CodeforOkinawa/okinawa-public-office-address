32515ab09427aa0432d480611829cdb57258b8c7
Seiei Higa <hanachin@gmail.com>
2014-05-23 22:45:53 +0900

    Wikipediaから沖縄の市町村一覧をとるようにした

--- get-addresses.rb
+++ get-addresses.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
-
-# ./get-addresses.rb > get-lat-lng.rb
-
 require 'rubygems'
 require 'bundler/setup'
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
 
-public_office_addresses_url = 'http://www.pref.okinawa.jp/handbook/No504.html'
-html = open(public_office_addresses_url).read.force_encoding('CP932').scrub.encode('UTF-8')
+public_office_addresses_url = 'http://ja.wikipedia.org/wiki/Template:%E6%B2%96%E7%B8%84%E7%9C%8C%E3%81%AE%E8%87%AA%E6%B2%BB%E4%BD%93'
+html = open(public_office_addresses_url).read.force_encoding('UTF-8').scrub.encode('UTF-8')
 doc = Nokogiri::HTML(html)
 
-rows = doc.xpath('/html/body/table[3]/tbody/tr/td/div/table[2]/tbody//tr').drop(1)
-puts rows.map{|tr| tr.search('td')}.map{|(city, _, address, _)| [city.text.strip, address.text.strip].join(',') }
+local_govs = doc.xpath('//*[contains(@class, "collapsible")]//tr//td[2]//a').to_a
+local_govs.map! {|a|
+  [
+    a.text,
+    URI.join(public_office_addresses_url, a[:href]).to_s
+  ]
+}
+
+CSV.open('get-lat-lng.rb', 'wb') do |csv|
+  local_govs.each {|g| csv << g }
+end
